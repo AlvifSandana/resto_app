64e5e4a6746cd4c0e16dacc10c19a4e146a4809e
AlvifSandana <asm240800@gmail.com>
2021-08-14 15:40:42 +0700

    new methods: search and post review

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:resto_app/models/create_review_response_model.dart';
 import 'package:resto_app/models/restaurant_detail_model.dart';
 import 'package:resto_app/models/restaurant_model.dart';
+import 'package:resto_app/models/restaurant_search_response_model.dart';
 
 class ApiService {
   static final String _baseUrl = 'https://restaurant-api.dicoding.dev/';
@@ -23,6 +25,34 @@ class ApiService {
       return RestaurantDetailModel.fromJson(json.decode(response.body));
     } else {
       throw Exception('Failed to load detail of restaurant');
+    }
+  }
+
+  Future<RestaurantSearchResponseModel> searchRestaurant(String query) async {
+    final response =
+        await http.get(Uri.parse(_baseUrl + 'search?query=' + query));
+    if (response.statusCode == 200) {
+      return RestaurantSearchResponseModel.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to search restaurant');
+    }
+  }
+
+  Future<CreateReviewResponseModel> postReview(
+      String id, String name, String review) async {
+    final response = await http.post(
+      Uri.parse(_baseUrl + 'review'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'X-Auth-Token': _apiKey
+      },
+      body: jsonEncode(
+          <String, String>{'id': id, 'name': name, 'review': review}),
+    );
+    if (response.statusCode == 201) {
+      return CreateReviewResponseModel.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to add new review');
     }
   }
 }
