8e41e4760c11fa6ad318c56bd12c5997abf08f62
AlvifSandana <asm240800@gmail.com>
2021-08-14 21:29:46 +0700

    add custom widget card restaurant

---
+++ lib/widgets/card_restaurant.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:resto_app/models/restaurant_model.dart';
+import 'package:resto_app/pages/restaurant_detail_page.dart';
+
+class CardRestaurant extends StatelessWidget {
+  final Restaurant restaurant;
+
+  const CardRestaurant({required this.restaurant});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: ListTile(
+        contentPadding: 
+          const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        leading: Hero(
+          tag: restaurant.pictureId, 
+          child: Image.network(
+            "https://restaurant-api.dicoding.dev/images/small/${restaurant.pictureId}",
+            width: 100,
+          ),
+        ),
+        title: Text(
+          restaurant.name,
+        ),
+        subtitle: Text(
+          restaurant.city
+        ),
+        onTap: () => Navigator.pushNamed(
+          context, 
+          RestaurantDetailPage.routeName,
+        ),
+      ),
+    );
+  }
+}
