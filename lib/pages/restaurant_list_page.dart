d31191ff4b6c70741e26e2dd6c8750b3151a3880
AlvifSandana <asm240800@gmail.com>
2021-08-14 21:29:34 +0700

    use provder

--- lib/pages/restaurant_list_page.dart
+++ lib/pages/restaurant_list_page.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:resto_app/provider/restaurant_provider.dart';
+import 'package:resto_app/widgets/card_restaurant.dart';
 
 class RestaurantListPage extends StatelessWidget {
-  const RestaurantListPage({ Key? key }) : super(key: key);
+  const RestaurantListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Consumer<RestaurantProvider>(
+      builder: (context, state, _) {
+        if (state.state == ResultState.Loading) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state.state == ResultState.HasData) {
+          return ListView.builder(
+            itemCount: state.restaurant.count,
+            itemBuilder: (context, index) {
+              var restaurant = state.restaurant.restaurants[index];
+              return CardRestaurant(restaurant: restaurant);
+            },
+          );
+        } else if (state.state == ResultState.NoData) {
+          return Center(
+            child: Text(state.message),
+          );
+        } else if (state.state == ResultState.Error) {
+          return Center(
+            child: Text(state.message),
+          );
+        } else {
+          return Center(
+            child: Text(''),
+          );
+        }
+      },
     );
   }
 }
