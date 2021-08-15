571f9e99a938aecb0a861fc10d4be476cb467d3a
AlvifSandana <asm240800@gmail.com>
2021-08-16 01:46:20 +0700

    fix args navigator

--- lib/widgets/card_restaurant.dart
+++ lib/widgets/card_restaurant.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:resto_app/common/restaurant_detail_page_args.dart';
 import 'package:resto_app/models/restaurant_model.dart';
 import 'package:resto_app/pages/restaurant_detail_page.dart';
 
@@ -11,10 +12,10 @@ class CardRestaurant extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: ListTile(
-        contentPadding: 
-          const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
         leading: Hero(
-          tag: restaurant.pictureId, 
+          tag: restaurant.pictureId,
           child: Image.network(
             "https://restaurant-api.dicoding.dev/images/small/${restaurant.pictureId}",
             width: 100,
@@ -23,14 +24,11 @@ class CardRestaurant extends StatelessWidget {
         title: Text(
           restaurant.name,
         ),
-        subtitle: Text(
-          restaurant.city
-        ),
-        onTap: () => Navigator.pushNamed(
-          context, 
-          RestaurantDetailPage.routeName,
-          arguments: restaurant,
-        ),
+        subtitle: Text(restaurant.city),
+        onTap: () {
+          Navigator.pushNamed(context, RestaurantDetailPage.routeName,
+              arguments: RestaurantDetailPageArguments(id: restaurant.id));
+        },
       ),
     );
   }
